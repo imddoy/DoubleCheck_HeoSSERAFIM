de62fe91911fad7c177df10d7a509b36eb04343a
DAASHeo <hibstar719@naver.com>
2023-08-06 20:07:54 +0900

    프로젝트 기본 구조 세팅

--- double-check/src/App.js
+++ double-check/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Reset } from "styled-reset";
+import Main from "./pages/MainPage";
+import ReportList from "./pages/ReportListPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <>
+        <Reset />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/report" element={<ReportList />} />
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
---
+++ double-check/src/pages/ReportListPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ReportListPage() {
+  return <div>ReportListPage</div>;
+}
+
+export default ReportListPage;
