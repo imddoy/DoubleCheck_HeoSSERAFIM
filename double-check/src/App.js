94001302478bea806a225a3870b0a64bf72d4a9e
harim061 <doris5093@duksung.ac.kr>
2023-08-06 20:43:11 +0900

    [0806] 리미 페이지 생성

--- double-check/src/App.js
+++ double-check/src/App.js
@@ -1,20 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Reset } from "styled-reset";
-import Main from "./pages/MainPage";
-import ReportList from "./pages/ReportListPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import Main from './pages/MainPage';
+import ReportList from './pages/ReportListPage';
+import Fakenews from './pages/Fakenews';
+import Trends from './pages/Trends';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <>
-        <Reset />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/report" element={<ReportList />} />
-        </Routes>
-      </>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <>
+                <Reset />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/report" element={<ReportList />} />
+                    <Route path="/fakenews" element={<Fakenews />} />
+                    <Route path="/trend" element={<Trends />} />
+                </Routes>
+            </>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ double-check/src/pages/Fakenews.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Fakenews() {
+    return <div>Fakenews</div>;
+}
+
+export default Fakenews;
