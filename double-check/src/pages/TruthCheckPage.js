94a2e69ef7004fb06db357e8180a1acb01b89ec8
DAASHeo <hibstar719@naver.com>
2023-08-09 23:26:10 +0900

    피드백 전송 버트 구현

--- double-check/src/pages/TruthCheckPage.js
+++ double-check/src/pages/TruthCheckPage.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Div } from "../components/TruthCheck/TruthCheckStyle";
+import SatisfactionSlider from "../components/TruthCheck/SatisfactionSlider";
 
 function TruthCheckPage() {
-  return <div>TruthCheckPage</div>;
+  return (
+    <Div>
+      <SatisfactionSlider />
+    </Div>
+  );
 }
 
 export default TruthCheckPage;
