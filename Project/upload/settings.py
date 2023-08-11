14379e97a684808d30d8df2aa7db34fd5f1950ea
DAASHeo <hibstar719@naver.com>
2023-08-11 20:06:24 +0900

    [0811] 백과 1차 연결(신고 목록)

--- Project/upload/settings.py
+++ Project/upload/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +130,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
--- double-check/src/components/ReportList/ReportList.js
+++ double-check/src/components/ReportList/ReportList.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import default_img from "../../img/default_image.png";
+import axios from "axios";
 import {
   Fdiv,
   LBtn,
@@ -15,6 +17,18 @@ import {
 } from "./ReportListStyle";
 
 function ReportList() {
+  const [report, setReports] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/upload/")
+      .then((response) => {
+        setReports(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+  }, []);
+
   return (
     <>
       <Fdiv>허위뉴스를 신고해주세요</Fdiv>
@@ -24,7 +38,14 @@ function ReportList() {
         </Link>
         <LBox>
           <ListSmall>
-            <SBox>
+            {report.map((event) => (
+              <SBox key={event.id}>
+                <BTitle>{event.title}</BTitle>
+                <BText>{event.content}</BText>
+                <BBtn>더보기</BBtn>
+              </SBox>
+            ))}
+            {/* <SBox>
               <BTitle>사이버렉카 결국 수용</BTitle>
               <BText>
                 어쩌구 저쩌구 콘텐츠 몇줄 미리보기 최대 세 줄까지만 가능합니다.
@@ -47,15 +68,7 @@ function ReportList() {
                 테스트를 위해 겁나 많이 작성하고 있습니다 허은 최고
               </BText>
               <BBtn>더보기</BBtn>
-            </SBox>
-            <SBox>
-              <BTitle>사이버렉카 결국 수용</BTitle>
-              <BText>
-                어쩌구 저쩌구 콘텐츠 몇줄 미리보기 최대 세 줄까지만 가능합니다.
-                테스트를 위해 겁나 많이 작성하고 있습니다 허은 최고
-              </BText>
-              <BBtn>더보기</BBtn>
-            </SBox>
+            </SBox> */}
           </ListSmall>
           <ListBig>
             <BTitle>사이버렉카 결국 수용</BTitle>
