cdf95e68da85ab957b668efe9a5e5816c4d2a1f9
harim061 <doris5093@duksung.ac.kr>
2023-08-12 03:29:31 +0900

    [0812] get 수정

--- Project/upload/settings.py
+++ Project/upload/settings.py
@@ -150,9 +150,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-
-]
-
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = ('http://127.0.0.1:3000', 'http://localhost:3000')
+CORS_ALLOW_CREDENTIALS = True
