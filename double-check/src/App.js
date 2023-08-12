e0ff328ce9e6dae833bae1c6f27c2a5dfab381f2
harim061 <doris5093@duksung.ac.kr>
2023-08-13 01:50:07 +0900

    [0813] fake detail 완성

--- double-check/src/App.js
+++ double-check/src/App.js
@@ -1,31 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Reset } from "styled-reset";
-import Root from "./pages/root";
-import Main from "./pages/MainPage";
-import ReportList from "./pages/ReportListPage";
-import Fakenews from "./pages/Fakenews";
-import Trends from "./pages/Trends";
-import TruthCheck from "./pages/TruthCheckPage";
-import Nav from "./pages/NavPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import Root from './pages/root';
+import Main from './pages/MainPage';
+import ReportList from './pages/ReportListPage';
+import Fakenews from './pages/Fakenews';
+import Trends from './pages/Trends';
+import TruthCheck from './pages/TruthCheckPage';
+import Nav from './pages/NavPage';
+import FakeDetail from './pages/FakeDetail';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <>
-        <Reset />
-        <Routes>
-          <Route exact path="/" element={<Root />}>
-            <Route path="/" element={<Main />} />
-            <Route path="/report" element={<ReportList />} />
-            <Route path="/truthcheck" element={<TruthCheck />} />
-            <Route path="fakenews" element={<Fakenews />} />
-            <Route path="trend" element={<Trends />} />
-            <Route path="nav" element={<Nav />} />
-          </Route>
-        </Routes>
-      </>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <>
+                <Reset />
+                <Routes>
+                    <Route exact path="/" element={<Root />}>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/report" element={<ReportList />} />
+                        <Route path="/truthcheck" element={<TruthCheck />} />
+                        <Route path="fakenews" element={<Fakenews />} />
+                        <Route path="trend" element={<Trends />} />
+                        <Route path="nav" element={<Nav />} />
+                        <Route
+                            exact
+                            path="fakenews/:id"
+                            element={<FakeDetail />}
+                        />
+                    </Route>
+                </Routes>
+            </>
+        </BrowserRouter>
+    );
 }
 
 export default App;
