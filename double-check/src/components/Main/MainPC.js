ce69d4459ee8d620bef0396c08c52dad65c0aa87
harim061 <doris5093@duksung.ac.kr>
2023-08-13 04:01:28 +0900

    [0813] 메인 퍼블리싱 수정, verify  연결

--- double-check/src/components/Main/MainPC.js
+++ double-check/src/components/Main/MainPC.js
@@ -1,72 +1,85 @@
-import React from "react";
-import { styled } from "styled-components";
-import { Link } from "react-router-dom";
-import Filter from "./../Trends/Filter";
-
-const Main = styled.div`
-  padding: 5em 18em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-`;
-const Section = styled.div`
-  width: 100%;
-  margin-bottom: 100px;
-  color: var(--black, #000);
-
-  font-family: Pretendard;
-  font-size: 1.2em;
-  font-weight: 700;
-  line-height: normal;
-`;
-const Input = styled.div`
-  width: 100%;
-  height: 44px;
-  flex-shrink: 0;
-  border-radius: 10px;
-  border: 1px solid var(--ain, #3a42bf);
-  margin: 22px 0;
-  color: var(--ain, #3a42bf);
-
-  font-family: Pretendard;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: normal;
-
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 14px 20px;
-  box-shadow: 0px 8px 25px 0px rgba(13, 10, 44, 0.06);
-`;
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Img,
+    Input,
+    Input2,
+    InputBox,
+    ResponsiveImage,
+    Section,
+} from './MainStyle';
+import search from '../../img/search.png';
+import axios from 'axios';
+import mainImg from '../../img/main.png';
 
 function MainPC() {
-  return (
-    <Main>
-      <Section>
-        <p>사실 여부를 판단하고자 하는 </p>
-        <p>미디어의 링크를 붙여 넣어 주세요</p>
-        <Input>
-          <p>링크를 입력하세요</p>
-        </Input>
-      </Section>
-      <Section>
-        <p>이미 검증된 허위 뉴스를 찾아 보세요</p>
-        <Link to="/search">
-          <Input>
-            <p>찾고자 하는 이슈의 키워드를 입력하세요</p>
-            <img src="./search.png" />
-          </Input>
-        </Link>
-      </Section>
-      <Section>
-        <p style={{ margin: "22px 0" }}>현재 가장 많이 분석되고 있어요</p>
-        <Filter />
-      </Section>
-    </Main>
-  );
+    const [urlData, setUrlData] = useState('');
+    const [isClicked, setIsClicked] = useState(false);
+
+    function handleClick() {
+        setIsClicked(true);
+    }
+
+    const handleUrl = (e) => {
+        setUrlData(e.target.value);
+    };
+
+    const postUrl = (e) => {
+        e.preventDefault();
+
+        const jsonData = JSON.stringify({ youtube_url: urlData });
+
+        axios
+            .post('http://127.0.0.1:8000/verify/', jsonData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.log('작성 실패');
+                console.log(error.message);
+                console.log(error);
+                if (error.response && error.response.data) {
+                    console.log(error.response.data);
+                }
+            });
+    };
+
+    return (
+        <>
+            <ResponsiveImage src={mainImg} alt="main" />
+            <Section>
+                <p>사실 여부를 판단하고자 하는 </p>
+                <p>미디어의 링크를 붙여 넣어 주세요</p>
+                <InputBox isClicked={isClicked}>
+                    <Input
+                        onChange={handleUrl}
+                        onClick={handleClick}
+                        placeholder="링크를 입력하세요"
+                    />
+                    <Img src={search} onClick={postUrl} />
+                </InputBox>
+            </Section>
+            <Section mb="100px">
+                <p>이미 검증된 허위 뉴스를 찾아 보세요</p>
+                <Link to="/search">
+                    <Input2>
+                        <p>찾고자 하는 이슈의 키워드를 입력하세요</p>
+                        <img src="./search.png" />
+                    </Input2>
+                </Link>
+            </Section>
+            {/* <Section>
+                <p style={{ margin: '22px 0' }}>
+                    현재 가장 많이 분석되고 있어요
+                </p>
+                <Filter />
+            </Section> */}
+        </>
+    );
 }
 
 export default MainPC;
