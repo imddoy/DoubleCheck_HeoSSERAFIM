95a469179842c88bda5571cd07cb8d91b8edbecb
kknari <nari907@duksung.ac.kr>
2023-08-14 02:37:58 +0900

    유튜브 프로필 가지고 오기(model 변경)

--- Project/uploadPage/models.py
+++ Project/uploadPage/models.py
@@ -3,16 +3,22 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=40)
-    target = models.CharField(max_length=400)
-    url = models.URLField()
+    target = models.CharField(max_length=200)
+    upload_url = models.URLField()
     content = models.TextField()
-    head_image = models.ImageField(upload_to='images/', blank=True, null=True)
+    head_image = models.ImageField(upload_to='images/', blank=True, null=True, default=(0.0))
     created_at = models.DateField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.target.startswith('https://www.youtube.com/'):
+            self.target = self.target[len('https://www.youtube.com/'):]
+        
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'{self.title}'
     
-    def save(self, *args, **kwargs):
-        # 입력받은 target 값에서 띄어쓰기 제거
-        self.target = self.target.replace(' ', '')
-        super(Post, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     # 입력받은 target 값에서 띄어쓰기 제거
+    #     self.target = self.target.replace(' ', '')
+    #     super(Post, self).save(*args, **kwargs)
