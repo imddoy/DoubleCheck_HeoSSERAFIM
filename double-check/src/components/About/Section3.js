c8395bf367e04c1714ab48088d20e99c0ff3683e
imddoy <imddoy@duksung.ac.kr>
2023-08-18 04:46:58 +0900

    흠

--- double-check/src/components/About/Section3.js
+++ double-check/src/components/About/Section3.js
@@ -1,18 +1,24 @@
-import React from 'react';
-import { D3iv, S3Div, Title3 } from './AboutStyle';
-import caputre from '../../img/chat.png';
-import use from '../../img/howto.png';
+import React from "react";
+import { D3iv, S3Div, Title3 } from "./AboutStyle";
+import caputre from "../../img/chat.png";
+import use from "../../img/howto.png";
+import hmm from "../../img/hmm.gif";
 function Section3() {
-    return (
-        <S3Div mt="200px">
-            <Title3>
-                DOUBLE CHECK, <span>이렇게 나오게 되었습니다</span>
-            </Title3>
+  return (
+    <S3Div mt="200px" style={{ position: "relative" }}>
+      <Title3>
+        DOUBLE CHECK, <span>이렇게 나오게 되었습니다</span>
+      </Title3>
 
-            <D3iv src={caputre} />
-            <D3iv src={use} />
-        </S3Div>
-    );
+      <D3iv src={caputre} />
+      <img
+        src={hmm}
+        width={"10%"}
+        style={{ position: "absolute", top: "20%", right: "10px" }}
+      />
+      <D3iv src={use} />
+    </S3Div>
+  );
 }
 
 export default Section3;
