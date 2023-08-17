e22ad5602480bff7706af50e11e63de29c619fc1
harim061 <doris5093@duksung.ac.kr>
2023-08-18 01:03:49 +0900

    [0818] about 완성

--- double-check/src/components/About/Section1.js
+++ double-check/src/components/About/Section1.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import Com from '../../img/com.svg';
 import Light from '../../img/light.svg';
-import { Limg, S1Div, S1Div2, STitle, Title } from './AboutStyle';
+import { Cimg, Limg, S1Div, S1Div2, STitle, Title } from './AboutStyle';
 
 function Section1() {
     return (
         <S1Div>
-            <img src={Com} />
+            <Cimg src={Com} />
 
             <S1Div2>
-                <Limg src={Light} />
                 <STitle>
                     인공지능의 눈과 당신의 눈 <br />
                     더블체크로 확실하게!
