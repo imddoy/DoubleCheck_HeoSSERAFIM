aa7a858f3fb3cb1d2afcf1649cd05058ff2328bd
harim061 <doris5093@duksung.ac.kr>
2023-08-17 22:42:08 +0900

    about 반응형 아직

--- double-check/src/components/About/Section4.js
+++ double-check/src/components/About/Section4.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { S4Div, S4Title } from './AboutStyle';
+import { Gobtn, S4Div, S4Title } from './AboutStyle';
+import phone from '../../img/phone.svg';
 
 function Section4() {
     return (
@@ -7,6 +8,10 @@ function Section4() {
             <S4Title>
                 기다릴 필요 없어요. 더블체크와 지금 바로 시작하세요!
             </S4Title>
+
+            <img src={phone} />
+
+            <Gobtn>지금 double check 하기</Gobtn>
         </S4Div>
     );
 }
