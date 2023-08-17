f34a2996a6861c904e3e77af1fa2fcace43ed77a
harim061 <doris5093@duksung.ac.kr>
2023-08-17 21:55:25 +0900

    [0817] about 만드는 중

--- double-check/src/App.js
+++ double-check/src/App.js
@@ -1,37 +1,44 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Reset } from "styled-reset";
-import Root from "./pages/root";
-import Main from "./pages/MainPage";
-import ReportList from "./pages/ReportListPage";
-import Fakenews from "./pages/Fakenews";
-import Trends from "./pages/Trends";
-import TruthCheck from "./pages/TruthCheckPage";
-import Nav from "./pages/NavPage";
-import FakeDetail from "./pages/FakeDetail";
-import Search from "./pages/SearchPage";
-import Check from "./pages/CheckPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Reset } from 'styled-reset';
+import Root from './pages/root';
+import Main from './pages/MainPage';
+import ReportList from './pages/ReportListPage';
+import Fakenews from './pages/Fakenews';
+import Trends from './pages/Trends';
+import TruthCheck from './pages/TruthCheckPage';
+import Nav from './pages/NavPage';
+import FakeDetail from './pages/FakeDetail';
+import Search from './pages/SearchPage';
+import Check from './pages/CheckPage';
+import About from './pages/About';
+
 function App() {
-  return (
-    <BrowserRouter>
-      <>
-        <Reset />
-        <Routes>
-          <Route exact path="/" element={<Root />}>
-            <Route path="/" element={<Main />} />
-            <Route path="/report" element={<ReportList />} />
-            <Route path="/check" element={<Check />} />
-            <Route path="/truthcheck" element={<TruthCheck />} />
-            <Route path="fakenews" element={<Fakenews />} />
-            <Route path="trend" element={<Trends />} />
-            <Route path="nav" element={<Nav />} />
-            <Route path="search" element={<Search />} />
-            <Route exact path="fakenews/:id" element={<FakeDetail />} />
-            <Route exact path="/trend/:id" element={<Trends />} />
-          </Route>
-        </Routes>
-      </>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <>
+                <Reset />
+                <Routes>
+                    <Route exact path="/" element={<Root />}>
+                        <Route path="/" element={<Main />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/report" element={<ReportList />} />
+                        <Route path="/check" element={<Check />} />
+                        <Route path="/truthcheck" element={<TruthCheck />} />
+                        <Route path="fakenews" element={<Fakenews />} />
+                        <Route path="trend" element={<Trends />} />
+                        <Route path="nav" element={<Nav />} />
+                        <Route path="search" element={<Search />} />
+                        <Route
+                            exact
+                            path="fakenews/:id"
+                            element={<FakeDetail />}
+                        />
+                        <Route exact path="/trend/:id" element={<Trends />} />
+                    </Route>
+                </Routes>
+            </>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ double-check/src/components/About/AboutStyle.js
@@ -0,0 +1,118 @@
+import { styled } from 'styled-components';
+
+export const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #141414;
+`;
+
+/* Section1 */
+export const S1Div = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: ${(props) => props.mt};
+`;
+export const STitle = styled.div`
+    color: #fff;
+    text-align: center;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`;
+
+export const Title = styled.div`
+    color: #fff;
+    text-align: center;
+    font-size: 80px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+    letter-spacing: -2px;
+`;
+
+export const S1Div2 = styled.div`
+    position: relative; // 추가
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    align-self: center;
+    margin: 0 auto;
+    justify-content: center;
+`;
+
+export const Limg = styled.img`
+    position: absolute;
+    top: -210px;
+    left: 370px; // 원하는 위치로 조절
+`;
+
+/* Section2 */
+export const Title2 = styled.div`
+    color: #fff;
+    font-family: Pretendard;
+    font-size: 60px;
+    font-style: normal;
+    font-weight: ${(props) => props.fw};
+    line-height: normal;
+    letter-spacing: -6px;
+`;
+
+export const TitleDiv = styled.div`
+    margin-left: 130px;
+`;
+
+export const LogoImg = styled.img`
+    align-self: center;
+    margin: 0 auto;
+`;
+
+/* Section3 */
+export const Title3 = styled.div`
+    color: #fff;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 60px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: normal;
+    letter-spacing: -6px;
+    margin: 0 auto;
+    span {
+        font-weight: 800;
+    }
+`;
+export const S3Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: ${(props) => props.mt};
+    padding-bottom: 100px;
+`;
+
+export const D3iv = styled.img`
+    width: 100%;
+`;
+
+/* Section4 */
+
+export const S4Div = styled.div`
+    background-color: #3a42bf;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: ${(props) => props.mt};
+`;
+
+export const S4Title = styled.div`
+    color: #fff;
+    text-align: center;
+    font-size: 80px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`;
